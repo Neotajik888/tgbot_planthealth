90f78f4b94ccc0e611d016aee4fa393f3b27df38
Neotajik888 <misha1011nat@gmail.com>
2025-10-05 18:47:10 +0300

    bnt

--- bot/bot.py
+++ bot/bot.py
@@ -11,6 +11,12 @@ def message_received(message):
     print(message)
     bot.send_message(chat_id=message.from_user.id, text="привет, " + message.from_user.first_name)
 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🇷🇺 Русский")
+    btn2 = types.KeyboardButton('🇬🇧 English')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
+
 
 
 bot.polling(none_stop=True, interval=0)
